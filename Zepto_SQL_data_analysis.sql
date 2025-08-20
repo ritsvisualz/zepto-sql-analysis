640978dbfb1c2c0cf1f5447107deab24b5b85b55
ritsvisualz <107040591+ritsvisualz@users.noreply.github.com>
2025-08-20 23:46:13 +0530

    Update Zepto_SQL_data_analysis.sql

--- Zepto_SQL_data_analysis.sql
+++ Zepto_SQL_data_analysis.sql
@@ -13,6 +13,7 @@ outOfStock BOOLEAN,
 quantity INTEGER
 );
 
+
 --data exploration
 
 --count of rows
@@ -21,6 +22,7 @@ select count(*) from zepto;
 --sample data
 SELECT * FROM zepto
 LIMIT 10;
+
 
 --null values
 SELECT * FROM zepto
@@ -42,15 +44,18 @@ outOfStock IS NULL
 OR
 quantity IS NULL;
 
+
 --different product categories
 SELECT DISTINCT category
 FROM zepto
 ORDER BY category;
 
+
 --products in stock vs out of stock
 SELECT outOfStock, COUNT(sku_id)
 FROM zepto
 GROUP BY outOfStock;
+
 
 --product names present multiple times
 SELECT name, COUNT(sku_id) AS "Number of SKUs"
@@ -58,6 +63,7 @@ FROM zepto
 GROUP BY name
 HAVING count(sku_id) > 1
 ORDER BY count(sku_id) DESC;
+
 
 --data cleaning
 
@@ -68,12 +74,15 @@ WHERE mrp = 0 OR discountedSellingPrice = 0;
 DELETE FROM zepto
 WHERE mrp = 0;
 
+
 --convert paise to rupees
 UPDATE zepto
 SET mrp = mrp / 100.0,
 discountedSellingPrice = discountedSellingPrice / 100.0;
 
 SELECT mrp, discountedSellingPrice FROM zepto;
+
+
 
 --data analysis
 
@@ -90,12 +99,14 @@ FROM zepto
 WHERE outOfStock = TRUE and mrp > 300
 ORDER BY mrp DESC;
 
+
 --Q3.Calculate Estimated Revenue for each category
 SELECT category,
 SUM(discountedSellingPrice * availableQuantity) AS total_revenue
 FROM zepto
 GROUP BY category
 ORDER BY total_revenue;
+
 
 -- Q4. Find all products where MRP is greater than ₹500 and discount is less than 10%.
 SELECT DISTINCT name, mrp, discountPercent
@@ -111,12 +122,14 @@ GROUP BY category
 ORDER BY avg_discount DESC
 LIMIT 5;
 
+
 -- Q6. Find the price per gram for products above 100g and sort by best value.
 SELECT DISTINCT name, weightInGms, discountedSellingPrice,
 ROUND(discountedSellingPrice/weightInGms,2) AS price_per_gram
 FROM zepto
 WHERE weightInGms >= 100
 ORDER BY price_per_gram;
+
 
 --Q7.Group the products into categories like Low, Medium, Bulk.
 SELECT DISTINCT name, weightInGms,
@@ -125,6 +138,7 @@ CASE WHEN weightInGms < 1000 THEN 'Low'
 	ELSE 'Bulk'
 	END AS weight_category
 FROM zepto;
+
 
 --Q8.What is the Total Inventory Weight Per Category 
 SELECT category,
